22f04ac67a3d11d4d11343f6858c0a947e158601
Alex <medinas.alex0001@gmail.com>
2018-09-10 10:07:24 -0500

    Se creo la Prac1 sobre el servidio de transferencia de archivos, se implemento la interfaz y la parte del cliente, la parte del servidor y enviar el tamaño del buffer así como la activacion del algoritmo de Nagle

---
+++ Practica1/src/practica1/ClienteArchivo.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1;
+
+import javax.swing.JFileChooser;
+import java.net.*;
+import java.io.*;
+
+/**
+ *
+ * @author alejandroms
+ */
+public class ClienteArchivo {
+    File[] file;
+    int tam_buffer;
+    
+    public ClienteArchivo(File[] f, String buffer){
+        this.file = f;
+        this.tam_buffer = Integer.parseInt(buffer);
+    }
+    
+    public void enviaArchivos(){
+        try{
+            Socket cl = new Socket("127.0.0.1", 7000);
+            DataOutputStream dos = new DataOutputStream(cl.getOutputStream());
+            DataInputStream dis = null;
+            int i = 0;
+            int num_archivos = file.length;
+            dos.writeInt(num_archivos);
+            dos.flush();
+            String archivo = "";
+            String nombre = "";
+            long tam = 0;
+            System.out.println("Núm archivos: " + file.length);
+            while(i < file.length){ 
+                archivo = file[i].getAbsolutePath();
+                nombre = file[i].getName();
+                tam = file[i].length();
+                System.out.println("Archivo: " + file[i].getName());
+                System.out.println("Tamaño Archivo: " + tam);
+                dis = new DataInputStream(new FileInputStream(archivo));                    
+                dos.writeUTF(nombre);
+                dos.writeLong(tam);
+                dos.flush();                            
+                byte[] b = new byte[tam_buffer];
+                long enviados = 0;
+                int porcentaje, n;
+                while(enviados < tam){
+                    n = dis.read(b);                                              
+                    dos.write(b, 0, n);
+                    dos.flush();
+                    enviados = enviados + n;                        
+                    porcentaje = (int)(enviados * 100 / tam);
+                    System.out.print("Enviado: " + porcentaje + "%\r");
+                }
+                i++;
+                System.out.print("\n\nArchivo enviado\n");
+            }
+            dos.close();
+            dis.close();
+            cl.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+       
+}
